743e572333d04ac6f395b006ea65d498a7e523e6
Borbiro <43454691+Borbiro@users.noreply.github.com>
2026-07-21 21:01:10 +0200

    Add subject sql query

--- src/main/java/com/learningtracker/subject/SubjectRepository.java
+++ src/main/java/com/learningtracker/subject/SubjectRepository.java
@@ -1,9 +1,15 @@
 package com.learningtracker.subject;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import java.util.List;
 
 public interface SubjectRepository extends JpaRepository<Subject, Long> {
 
+    @Query(value = """
+        SELECT * FROM subjects s
+        ORDER BY s.name ASC 
+       """, nativeQuery = true)
     List<Subject> findAllByArchivedFalseOrderByNameAsc();
 }
